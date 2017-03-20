47d8b385690c3b75a6462486ed1b78634711eec8
lucky clover <clover@godking.local>
2017-03-20 22:14:20 +0400

    Initial Commit

---
+++ QuantumListing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuantumListing
+//
+//  Created by lucky clover on 3/20/17.
+//  Copyright © 2017 lucky clover. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
